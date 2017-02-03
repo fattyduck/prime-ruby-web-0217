0e6a454bfb879254286291774c5d572f08346bd2
dison ruan <dison.ruan@yahoo.com>
2017-02-03 17:10:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  return false if num < 2
+
+  return true if num < 4
+
+  return num % 2 != 0 if num < 8
+
+  for i in 2..10
+    return false if num % i == 0
+  end
+  true
+end
